33f413e19ce08e577c27e620f5041baf1ffb824a
jeniferGoncalvesDaSilvaDev <49965702+jeniferGoncalvesDaSilvaDev@users.noreply.github.com>
2023-05-29 21:33:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-Start
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-zmf7mx)
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './index.html'
+    ,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
